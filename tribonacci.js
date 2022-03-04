4aa2bfebf434e3e0c6ed426e443bd3b38d3034fb
Ryan M <ryan@ryanmo.co>
2022-03-04 14:27:22 -0700

    small update

--- tribonacci.js
+++ tribonacci.js
@@ -1,8 +1,7 @@
 function tribonacci(signature,n){
   if (n <= 3) {
     return signature.slice(0, n);
-  }
-  
+  } 
   let sequence = [...signature];
   for (let i = 0; i < n - 3; i++) {
     const newValue = sequence[i] + sequence[i + 1] + sequence[i + 2]
